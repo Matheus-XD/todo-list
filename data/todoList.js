2cc647ed76f38e40b2403a7f4a45537fda0fe5a4
Matheus de Sousa <matheus137872@gmail.com>
2026-07-19 13:18:31 -0300

    finishing todo list

--- data/todoList.js
+++ data/todoList.js
@@ -1,4 +1,33 @@
-export let todoList = []
+export let todoList = JSON.parse(localStorage.getItem('todoList')) || []
+
+export function renderHTML() {
+    let todoListHTML = ''
+    todoList.forEach((item)=>{
+        const todo = `
+            <div class="todo-container js-todo-container-${item.id}" data-todo-id = "${item.id}">
+                <p class="todo">${item.todo}</p>
+                <p class="date">${item.date}</p>
+                <button class="js-remove-button" data-todo-to-delete-id = "${item.id}">
+                    remove
+                </button>
+            </div>
+        `
+        todoListHTML += todo
+    })
+    
+    document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML
+
+    document.querySelectorAll('.js-remove-button')
+    .forEach((deleteButton)=>{
+        deleteButton.addEventListener('click', ()=>{
+            const todoToDeleteId = Number(deleteButton.dataset.todoToDeleteId); 
+            deleteFromList(todoToDeleteId)
+            deleteFromHtml(todoToDeleteId)
+            saveToLocalStorage(todoList)
+        })
+    })
+}
 
 export function deleteFromList(todoToDeleteID) {
     let newTodoList = []
@@ -8,9 +37,14 @@ export function deleteFromList(todoToDeleteID) {
         }
     })
     todoList = newTodoList;
-    console.log(todoList);
-    
-    
-    
-    
+    console.log(todoList);   
+}
+
+export function deleteFromHtml(todoToDeleteID) {
+    document.querySelector(`.js-todo-container-${todoToDeleteID}`)
+    .remove()
+}
+
+export function saveToLocalStorage(itemToSave) {
+    localStorage.setItem('todoList', JSON.stringify(itemToSave))
 }
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-import { todoList, deleteFromList} from "./data/todoList.js";
+import { todoList,renderHTML, deleteFromList, saveToLocalStorage} from "./data/todoList.js";
+
 
 let idNumber = 1
 document.querySelector('.js-add-button')
@@ -23,35 +24,38 @@ document.querySelector('.js-add-button')
         .value = null
         idNumber += 1
         renderHTML()
+        saveToLocalStorage(todoList)
+             
     }
     
 })
-function renderHTML() {
-    let todoListHTML = ''
-    todoList.forEach((item)=>{
-        const todo = `
-            <div class="todo-container js-todo-container-${item.id}" data-todo-id = "${item.id}">
-                <p class="todo">${item.todo}</p>
-                <p class="date">${item.date}</p>
-                <button class="js-remove-button" data-todo-to-delete-id = "${item.id}">
-                    remove
-                </button>
-            </div>
-        `
-        todoListHTML += todo
-    })
-    
-    document.querySelector('.js-todo-list')
-    .innerHTML = todoListHTML
 
-    document.querySelectorAll('.js-remove-button')
-    .forEach((deleteButton)=>{
-        deleteButton.addEventListener('click', ()=>{
-            const todoToDeleteId = deleteButton.dataset.todoToDeleteId; 
-            deleteFromList(todoToDeleteId)
-        })
-    })
-}
+document.querySelector('.js-todo-input')
+.addEventListener('keydown', (event)=>{
+    if (event.key === 'Enter') {
+        const todoItem = document.querySelector('.js-todo-input').value;
+        const dateItem = document.querySelector('.js-date-input').value;
+        if (!todoItem) {
+            alert('please insert a value')
+        } else {
+            todoList.push(
+                
+            )
+            console.log(todoList);
+            
+            document.querySelector('.js-todo-input') 
+            .value = null
+            document.querySelector('.js-date-input')
+            .value = null
+            idNumber += 1
+            renderHTML()
+            saveToLocalStorage(todoList)
+                
+        }
+    }
+    
+})
+
 
 
 
